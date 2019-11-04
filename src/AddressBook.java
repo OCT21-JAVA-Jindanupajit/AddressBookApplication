a1001fd204e3a3f9b27109ef0110835d3984e5a5
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-04 15:43:53 -0500

    Finish

---
+++ src/AddressBook.java
@@ -0,0 +1,4 @@
+import java.util.ArrayList;
+
+public class AddressBook extends ArrayList<Record> {
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Record record = new Record();
+
+
+    }
+
+}
